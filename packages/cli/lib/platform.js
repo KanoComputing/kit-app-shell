329a4b2c25428b0cb55f1734be2ddd1edf530df0
Paul Varache <paul@kano.me>
2018-12-03 09:42:23 +0000

    CLI WIP

---
+++ packages/cli/lib/platform.js
@@ -0,0 +1,16 @@
+function loadPlatform(name) {
+    if (name === 'common' || name === 'cli') {
+        throw new Error(`Could not load platform: '${name}' is reserved`);
+    }
+    let loaded;
+    try {
+        loaded = require(`@kano/kit-app-shell-${name}`);
+    } catch (e) {
+        throw new Error(`Could load platform: '${name}' was not installed`);
+    }
+    return loaded;
+}
+
+module.exports = {
+    loadPlatform,
+};
---
+++ packages/cli/lib/platform.test.js
@@ -0,0 +1,22 @@
+const { loadPlatform } = require('./platform');
+const { assert } = require('chai');
+const mock = require('mock-require');
+
+suite('resolve-platform', () => {
+    suite('loadPlatform', () => {
+        test('reserved names', () => {
+            assert.throws(() => loadPlatform('cli'));
+            assert.throws(() => loadPlatform('common'));
+        });
+        test('Explicit message when platform doesn\'t exist', () => {
+            assert.throws(() => loadPlatform('_'), `Could load platform: '_' was not installed`);
+        });
+        test('load correctly', () => {
+            const mod = Symbol();
+            mock('@kano/kit-app-shell-test-platform', mod);
+            const loadedModule = loadPlatform('test-platform');
+            assert.equal(loadedModule, mod);
+            mock.stop('@kano/kit-app-shell-test-platform');
+        });
+    });
+});
