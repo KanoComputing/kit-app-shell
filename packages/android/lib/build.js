6d3cf9420469fada9e46e0646e7da9f0bda46485
Paul Varache <perso@paulvarache.ninja>
2018-12-06 09:02:08 +0000

    Update

--- packages/android/lib/build.js
+++ packages/android/lib/build.js
@@ -1,15 +1,21 @@
 const { build } = require('@kano/kit-app-shell-cordova');
 const path = require('path');
 
+function getModulePath(name) {
+    return path.dirname(require.resolve(`${name}/package.json`));
+}
+
 module.exports = (opts, commandOpts) => {
 
     // Load plugins and platforms from the local dependencies.
     // This avoid using cordova-fetch and having to download deps on every build
-    const CORDOVA_ANDROID_PATH = path.dirname(require.resolve('cordova-android/package.json'));
-    const CORDOVA_BLUETOOTHLE_PLUGIN = path.dirname(require.resolve('cordova-plugin-bluetoothle/package.json'));
-
-    const platforms = [CORDOVA_ANDROID_PATH];
-    const plugins = [CORDOVA_BLUETOOTHLE_PLUGIN];
+    const pluginNames = [
+        'cordova-plugin-bluetoothle',
+        'cordova-plugin-device',
+        'cordova-plugin-splashscreen',
+    ];
+    const platforms = [getModulePath('cordova-android')];
+    const plugins = pluginNames.map(name => getModulePath(name));
 
     return build({
         ...opts,
