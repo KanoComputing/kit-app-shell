9dcfcbe4d950dc09213b5965427b247ca014ba49
Paul Varache <paul@kano.me>
2018-12-12 17:39:26 +0000

    Remove console.log

--- packages/electron/data/run.tpl.js
+++ packages/electron/data/run.tpl.js
@@ -1,5 +1,4 @@
 window.addEventListener('load', () => {
-    console.log('haha')
     const s = document.createElement('script');
     s.src = `${LR_URL}/livereload.js?snipver=1`;
     document.body.appendChild(s);
