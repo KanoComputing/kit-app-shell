2a996b69349f15bfdd1874e6bc63e6cdbedb4bdf
Delete me later <kano@Deletes-MacBook-Pro.local>
2019-01-24 21:48:31 +0000

    Disable v8 snapshot for macos

--- packages/electron/src/types.ts
+++ packages/electron/src/types.ts
@@ -1,6 +1,7 @@
 import { IBuildOptions } from '@kano/kit-app-shell-core/lib/types';
 
 export type ElectronBuildOptions = IBuildOptions & {
+    disableV8Snapshot? : boolean;
     bundle? : {
         patterns? : string[];
         forcePlatform? : string;
