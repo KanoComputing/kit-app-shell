c4b4d356b46360d9b703c83889178be16e2edf58
Radek Pazdera <radek@kano.me>
2019-03-06 17:04:38 +0000

    use xpc-connection resolution on mac only

--- packages/electron/tasks/postinstall.js
+++ packages/electron/tasks/postinstall.js
@@ -1,12 +1,20 @@
-const { resolve } = require('path');
+const { resolve, join } = require('path');
 const { execSync } = require('child_process');
 const { platform } = require('os');
+const { readFileSync, writeFileSync } = require('fs');
 
 const APP_PATH = resolve('./app');
 const BUILD_PARAMS = '--arch=x64 --target_arch=x64 --target=3.1.0 --runtime=electron --dist-url=https://atom.io/download/electron';
 const packagesToRebuild = ['noble-uwp'];
 
 if (platform() === 'darwin') {
+    /* Add an extra resollution for xpc-connection to resolve an issue with noble on macOS Mojave */
+    const packageJsonPath = join(APP_PATH, 'package.json');
+    const packageJson = readFileSync(packageJsonPath, { encoding: 'utf8' });
+    const pkg = JSON.parse(packageJson);
+    pkg.resolutions['xpc-connection'] = 'sandeepmistry/node-xpc-connection#26/head';
+    writeFileSync(packageJsonPath, `${JSON.stringify(pkg, null, '  ')}\n`);
+
     packagesToRebuild.push('xpc-connection');
 }
 
