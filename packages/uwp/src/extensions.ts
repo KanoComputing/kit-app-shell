464d8bca5a4c07100a64764fa8646f2c78b33e43
Paul Varache <paul@kano.me>
2019-04-29 17:47:25 +0100

    Add share target support

--- packages/uwp/src/extensions.ts
+++ packages/uwp/src/extensions.ts
@@ -1,6 +1,8 @@
 import { FileTypeAssociationProvider } from './extensions/file-type-association.js';
 import { IExtensionProvider } from './extensions/extension.js';
+import { ShareTargetProvider } from './extensions/share-target.js';
 
 export const extensions : { [K : string] : IExtensionProvider } = {
     FILE_TYPE_ASSOCIATION: FileTypeAssociationProvider,
+    SHARE_TARGET: ShareTargetProvider,
 };
