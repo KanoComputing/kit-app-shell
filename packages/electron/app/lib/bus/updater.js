ba515d4f72a94397437a588c3693669d7c829eed
Paul Varache <paul@kano.me>
2019-09-18 14:50:46 +0100

    fix: updater now works

--- packages/electron/app/lib/bus/updater.js
+++ packages/electron/app/lib/bus/updater.js
@@ -1,58 +1,56 @@
 const { app } = require('electron');
+const { ChannelServer } = require('@kano/web-bus/cjs/server');
+const { UpdaterChannelId, UpdaterMethod } = require('@kano/web-bus/cjs/apis/updater/definition');
 const createUpdater = require('../updater');
 
 let updater;
 
-function response(event, data) {
-    return {
-        eventId: event.eventId,
-        data
-    };
+class UpdaterServer extends ChannelServer {
+    constructor(bus, log) {
+        super(bus, UpdaterChannelId);
+        this.log = log;
+
+        this.currentCallback = null;
+
+        this.listen(UpdaterMethod.SETUP, req => this.setup(req.params[0]));
+        this.listen(UpdaterMethod.CHECK_FOR_UPDATES, () => this.checkForUpdates());
+        this.listen(UpdaterMethod.QUIT_AND_INSTALL, () => this.quitAndInstall());
+    }
+    setup(config) {
+        if (updater) {
+            this.log.trace('Updater already setup');
+            return Promise.resolve(false);
+        }
+        updater = createUpdater(config);
+        this.log.trace('Updater setup success');
+        updater.on('checking-for-update', (...args) => this.emit(UpdaterMethod.CHECKING_FOR_UPDATE_EVENT, ...args));
+        updater.on('update-available', (...args) => this.emit(UpdaterMethod.UPDATE_AVAILABLE_EVENT, ...args));
+        updater.on('update-not-available', (...args) => this.emit(UpdaterMethod.UPDATE_NOT_AVAILABLE_EVENT, ...args));
+        updater.on('update-downloaded', (...args) => this.emit(UpdaterMethod.UPDATE_DOWNLOADED_EVENT, ...args));
+        updater.on('before-quit-for-update', (...args) => this.emit(UpdaterMethod.BEFORE_QUIT_FOR_UPDATE_EVENT, ...args));
+        return Promise.resolve(true);
+    }
+    checkForUpdates() {
+        if (!updater) {
+            return Promise.reject(new Error('Could not check for updates: updater not setup'));
+        }
+        this.log.info('Checking for updates');
+        updater.checkForUpdates();
+        return Promise.resolve();
+    }
+    quitAndInstall() {
+        if (!updater) {
+            return Promise.reject(new Error('Could not quit and install: updater not setup'));
+        }
+        this.log.info('Quit and install requested');
+        if (process.platform === 'win32') {
+            app.quit();
+        }
+        updater.quitAndInstall();
+        return Promise.resolve();
+    }
 }
 
 module.exports = (bus, log) => {
-    bus.on('req', (event) => {
-        switch (event.name) {
-        case 'setup': {
-            if (updater) {
-                log.trace('Updater already setup');
-                bus.emit('res', response(event, false));
-                return;
-            }
-            updater = createUpdater(event.data);
-            bus.emit('res', response(event, true));
-            log.trace('Updater setup success');
-            const events = [
-                'checking-for-update',
-                'update-available',
-                'update-not-available',
-                'update-downloaded',
-                'before-quit-for-update',
-            ];
-            events.forEach(e => updater.on(e, (...args) => bus.emit(e, ...args)));
-            break;
-        }
-        case 'check-for-updates': {
-            if (!updater) {
-                return;
-            }
-            log.info('Checking for updates');
-            updater.checkForUpdates();
-            bus.emit('res', response(event));
-            break;
-        }
-        case 'quit-and-install': {
-            if (!updater) {
-                return;
-            }
-            log.info('Quit and install requested');
-            if (process.platform === 'win32') {
-                app.quit();
-            }
-            updater.quitAndInstall();
-            bus.emit('res', response(event));
-            break;
-        }
-        }
-    });
+    const s = new UpdaterServer(bus, log);
 };
