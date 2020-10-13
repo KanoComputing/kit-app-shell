86e56a0656052e1abbc48be1e3742646d145cb28
Joe Thom <joe.ethom@gmail.com>
2020-10-13 15:47:02 +0100

    wip tsconfig

--- packages/test/src/index.ts
+++ packages/test/src/index.ts
@@ -1,8 +1,7 @@
 import { loadPlatformKey } from '@kano/kit-app-shell-core/lib/util/platform';
 import { IBuilderFactory, ITestContext } from '@kano/kit-app-shell-core/lib/types';
-import * as wdjs from 'wd';
 
-export function getDriver(platform : string, wd : wdjs, ctx : ITestContext, opts) {
+export function getDriver(platform : string, wd : any, ctx : ITestContext, opts) {
     return loadPlatformKey(platform, 'test/get-builder')
         .then((getBuilder : IBuilderFactory) => {
             return getBuilder(wd, ctx, opts)
